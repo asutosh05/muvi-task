effb16cb5798778b5950f58d6609e6986fae7d77
Asutosh <asutosh407@gmail.com>
2018-07-30 21:18:30 +0530

    day 16

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .models import Content
 # Create your tests here.
+
+Class ContentTestCase(TestCase):
+    
+    def test_Create_Content(self):
+        Content.objects.create(owner=1,name="Django video",releaseDate='07/30/2018')
